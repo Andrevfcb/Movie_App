77b965a257d5f69b20b67be1c56ee89fa471d9ab
Adrian Majewski <andrevfcb@gmail.com>
2021-01-01 16:10:28 +0100

    added styles and movie description

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 const API_IMAGE = "https://image.tmdb.org/t/p/w1280"
 
-const Movie = ({title, poster_path, overview, popularity}) => {
+const Movie = ({title, poster_path, overview, vote_average}) => {
     return (
         <div className='movie'>
-            <img src={API_IMAGE + poster_path} />
+            <img src={API_IMAGE + poster_path} alt={title} />
+            <div className='movie_information'>
+                <h3>{title}</h3>
+                <span>{vote_average}</span>
+            </div>
+            <div className='movie_description'>
+                <h2>Overwiew:</h2>
+                <span>{overview}</span>
+            </div>
         </div>
     )
 }
